de3d3843df7752fa6abbdca3cb9da51bb7956299
Adeline <t_rainbow_8@hotmail.com>
2022-06-16 16:15:24 +0800

    b

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        //.binaryTarget(name: "XIBLoadable", url: "https://www.mobilityone.com.my/ios-library/XIBLoadable.xcframework.zip", checksum: "f28f466ba77de638b3796ae18f77bb1293d28e898288922fa318305bc17ba63b"),
-        .binaryTarget(name: "XIBLoadable", url: "https://github.com/adelinedog/xcframework.git", checksum: "f28f466ba77de638b3796ae18f77bb1293d28e898288922fa318305bc17ba63b")
+        .binaryTarget(name: "XIBLoadable", url: "https://www.mobilityone.com.my/ios-library/XIBLoadable.xcframework.zip", checksum: "f28f466ba77de638b3796ae18f77bb1293d28e898288922fa318305bc17ba63b"),
+        //.binaryTarget(name: "XIBLoadable", url: "https://github.com/adelinedog/xcframework.git", checksum: "f28f466ba77de638b3796ae18f77bb1293d28e898288922fa318305bc17ba63b")
         //.binaryTarget(name: "XIBLoadable", path: "./Sources/XIBLoadable.xcframework")
     ]
 )
