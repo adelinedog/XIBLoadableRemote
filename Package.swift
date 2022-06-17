820dc41785b95d8315a377f23755975f8ea6de32
Adeline <t_rainbow_8@hotmail.com>
2022-06-17 13:58:28 +0800

    dog

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "XIBLoadable",
+    name: "xcframework",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         //
         .library(
-            name: "XIBLoadable",
-            targets: ["XIBLoadable"]),
+            name: "xcframework",
+            targets: ["xcframework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "XIBLoadable", url: "https://www.mobilityone.com.my/iosLibrary/XIBLoadable.xcframework.zip", checksum: "f28f466ba77de638b3796ae18f77bb1293d28e898288922fa318305bc17ba63b"),
-        //.binaryTarget(name: "XIBLoadable", url: "https://github.com/adelinedog/xcframework.git", checksum: "f28f466ba77de638b3796ae18f77bb1293d28e898288922fa318305bc17ba63b")
+        //.binaryTarget(name: "XIBLoadable", url: "https://www.mobilityone.com.my/iosLibrary/XIBLoadable.xcframework.zip", checksum: "f28f466ba77de638b3796ae18f77bb1293d28e898288922fa318305bc17ba63b"),
+        .binaryTarget(name: "xcframework", url: "https://github.com/adelinedog/xcframework/releases/download/1.0.0.zip", checksum: "f28f466ba77de638b3796ae18f77bb1293d28e898288922fa318305bc17ba63b")
         //.binaryTarget(name: "XIBLoadable", path: "./Sources/XIBLoadable.xcframework")
     ]
 )
